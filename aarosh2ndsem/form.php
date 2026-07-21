aefbe8a740e85bba0c6c9c95318564d125cf69bf
sushant <115689963+sushant5101@users.noreply.github.com>
2026-07-21 02:10:14 +0000

    first commit

---
+++ aarosh2ndsem/form.php
@@ -0,0 +1,21 @@
+
+<form action="">
+    Name:
+    <input type="text" id="name" name="name" placeholder="Enter your name">
+
+    Age:
+    <input type="number" id="age" name="age" placeholder="Enter your age">
+
+    Gender:
+    <input type="radio" id="male" name="gender" value="male">
+    Male:
+
+    <input type="radio" id="female" name="gender" value="female">
+    Female:
+
+    Address:
+    <input type="text" id="address" name="address" placeholder="Enter your address">
+
+    <button type="submit">Submit</button>
+</form>
+
